994b059953958928bd25db197e3d11811987ed98
Jarwis <681jarwis@gmail.com>
2020-08-12 23:34:49 +0530

    changed the HTML for the site .

--- myvenv/mysite/urls.py
+++ myvenv/mysite/urls.py
@@ -12,10 +12,15 @@ Class-based views
 Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
+
 """
+
+from django.urls import path, include
 from django.conf.urls import url
 from django.contrib import admin
 
 urlpatterns = [
-    url(r'^admin/', admin.site.urls),
+    path('admin/', admin.site.urls),
+    path('',include('blog.urls')),
+
 ]
